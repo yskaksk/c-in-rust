4dd6130eddfe511776f6a02e2a7336ea107edc55
yskaksk <yskaksk@gmail.com>
2021-06-21 23:56:04 +0900

    node追加

--- lang/src/main.rs
+++ lang/src/main.rs
@@ -13,12 +13,33 @@ use TokenKind::{TK_RESERVED, TK_NUM};
 #[derive(Clone)]
 struct Token {
     kind: TokenKind,
-    //next: Option<&Token>,
     val: Option<u32>,
     str: Vec<char>
 }
 
+#[derive(Clone)]
+enum NodeKind {
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NUM
+}
+
+use NodeKind::{ND_ADD, ND_SUB, ND_MUL, ND_DIV, ND_NUM};
+
+#[derive(Clone)]
+struct Node {
+    kind: NodeKind,
+    lhs: Box<Option<Node>>,
+    rhs: Box<Option<Node>>,
+    val: Option<u32>
+}
+
 fn consume(tokens: &mut VecDeque<Token>, op: char) -> bool {
+    if tokens.len() < 1 {
+        return false
+    }
     let token = tokens.front().unwrap();
     match token.kind {
         TK_RESERVED => {
@@ -111,6 +132,84 @@ fn tokenize(chars: Vec<char>) -> VecDeque<Token> {
     return tokens
 }
 
+fn new_node(kind: NodeKind, lhs: Node, rhs: Node) -> Node {
+    return Node {
+        kind,
+        lhs: Box::new(Some(lhs)),
+        rhs: Box::new(Some(rhs)),
+        val: None
+    }
+}
+
+fn new_node_num(val: u32) -> Node {
+    return Node {
+        kind: ND_NUM,
+        lhs: Box::new(None),
+        rhs: Box::new(None),
+        val: Some(val)
+    }
+}
+
+fn expr(tokens: &mut VecDeque<Token>) -> Node {
+    let mut node: Node = mul(tokens);
+    loop {
+        if consume(tokens, '+') {
+            node = new_node(ND_ADD, node.clone(), mul(tokens));
+        } else if consume(tokens, '-') {
+            node = new_node(ND_SUB, node.clone(), mul(tokens));
+        } else {
+            return node
+        }
+    }
+}
+
+fn mul(tokens: &mut VecDeque<Token>) -> Node {
+    let mut node: Node = primary(tokens);
+    loop {
+        if consume(tokens, '*') {
+            node = new_node(ND_MUL, node.clone(), primary(tokens));
+        } else if consume(tokens, '/') {
+            node = new_node(ND_DIV, node.clone(), primary(tokens));
+        } else {
+            return node
+        }
+    }
+}
+
+fn primary(tokens: &mut VecDeque<Token>) -> Node {
+    if consume(tokens, '(') {
+        let node = expr(tokens);
+        expect(tokens, ')');
+        return node
+    } else {
+        return new_node_num(expect_number(tokens).unwrap())
+    }
+}
+
+fn gen(node: Node) {
+    match node.kind {
+        ND_NUM => {
+            println!("  push {}", node.val.unwrap());
+            return
+        },
+        _ => {
+            gen(node.lhs.unwrap());
+            gen(node.rhs.unwrap());
+            match node.kind {
+                ND_ADD => println!("  add rax, rdi"),
+                ND_SUB => println!("  sub rax, rdi"),
+                ND_MUL => println!("  imul rax, rdi"),
+                ND_DIV => {
+                    println!("  cqo");
+                    println!("  idiv rdi");
+                },
+                _ => unreachable!()
+            }
+            println!("  push rax");
+        }
+    }
+}
+
 fn main() {
 
     let args: Vec<String> = env::args().collect();
@@ -119,21 +218,25 @@ fn main() {
     }
     let chars: Vec<char> = args[1].chars().collect();
     let mut tokens = tokenize(chars);
+    let node = expr(&mut tokens);
 
     println!(".intel_syntax noprefix");
     println!(".global main");
     println!("main:");
 
-    println!("  mov rax, {}", expect_number(&mut tokens).unwrap());
+    gen(node);
 
-    while !tokens.is_empty() {
-        if consume(&mut tokens, '+') {
-            println!("  add rax, {}", expect_number(&mut tokens).unwrap());
-            continue
-        }
-        expect(&mut tokens, '-');
-        println!("  sub rax, {}", expect_number(&mut tokens).unwrap());
-    }
+    //println!("  mov rax, {}", expect_number(&mut tokens).unwrap());
 
+    //while !tokens.is_empty() {
+    //    if consume(&mut tokens, '+') {
+    //        println!("  add rax, {}", expect_number(&mut tokens).unwrap());
+    //        continue
+    //    }
+    //    expect(&mut tokens, '-');
+    //    println!("  sub rax, {}", expect_number(&mut tokens).unwrap());
+    //}
+
+    println!("  pop rax");
     println!("  ret");
 }
